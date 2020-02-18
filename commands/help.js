572c1fe87975eb703ee3e70139efc815862f91fa
Amber Holly <halcyonsilver@gmail.com>
2020-02-18 18:09:00 +0000

    Add eslint
    - Add eslint as a dev dependency
    - Add lint script to package.json
    - Lint all files

--- commands/help.js
+++ commands/help.js
@@ -1,47 +1,60 @@
-const { prefix } = require('../config.json');
+const { prefix } = require("../config.json");
 
 module.exports = {
-	name: 'help',
-	description: 'List all of my commands or info about a specific command.',
-	aliases: ['h', 'halp'],
-	usage: '[command name]',
+	name: "help",
+	description: "List all of my commands or info about a specific command.",
+	aliases: ["h", "halp"],
+	usage: "[command name]",
 	cooldown: 5,
 	execute(message, args) {
 		const data = [];
 		const { commands } = message.client;
 
 		if (!args.length) {
-			data.push('💖 **Here\'s a list of all my commands:**\n');
-			data.push(commands.map(command => `\`${command.name}\``).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			data.push("💖 **Here's a list of all my commands:**\n");
+			data.push(commands.map(command => `\`${command.name}\``).join(", "));
+			data.push(
+				`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`
+			);
 
-			return message.author.send(data, { split: true })
+			return message.author
+				.send(data, { split: true })
 				.then(() => {
-					if (message.channel.type === 'dm') return;
-					message.channel.send('💖 **I\'ve sent you a message with all my commands!~**');
+					if (message.channel.type === "dm") return;
+					message.channel.send(
+						"💖 **I've sent you a message with all my commands!~**"
+					);
 				})
 				.catch(error => {
-					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-					message.channel.send('💔 **It seems like I can\'t DM you!**');
+					console.error(
+						`Could not send help DM to ${message.author.tag}.\n`,
+						error
+					);
+					message.channel.send("💔 **It seems like I can't DM you!**");
 				});
 		}
 
 		const name = args[0].toLowerCase();
-		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+		const command =
+      commands.get(name) ||
+      commands.find(c => c.aliases && c.aliases.includes(name));
 
 		if (!command) {
-			return message.reply('❣ **That\'s not a valid command!**');
+			return message.reply("❣ **That's not a valid command!**");
 		}
 
 		data.push(`💖 **Cutiebot Help~**\n`);
 		data.push(`**Command:** \`${command.name}\``);
 
-		if (command.aliases) data.push(`**Aliases:** \`${command.aliases.join('\`, \`')}\``);
-		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** \`${prefix}${command.name}\` \`${command.usage}\``);
+		if (command.aliases)
+			data.push(`**Aliases:** \`${command.aliases.join("`, `")}\``);
+		if (command.description)
+			data.push(`**Description:** ${command.description}`);
+		if (command.usage)
+			data.push(`**Usage:** \`${prefix}${command.name}\` \`${command.usage}\``);
 
 		data.push(`**Cooldown:** \`${command.cooldown || 3}\` second(s).`);
 
 		message.channel.send(data, { split: true });
-	},
+	}
 };
--- commands/say.js
+++ commands/say.js
@@ -1,26 +1,25 @@
 module.exports = {
-	name: 'say',
-	description: 'Says anything you want in a channel of your choice!',
-	usage: '[#channel][message]',
+	name: "say",
+	description: "Says anything you want in a channel of your choice!",
+	usage: "[#channel][message]",
 	cooldown: 0,
 	guildOnly: true,
 	execute(message, args) {
 		const chanId = message.mentions.channels.first();
-	  let thingToSay;
+		let thingToSay;
 
-	  if (!args[0]) {
+		if (!args[0]) {
 			return message.channel.send("❣ **You need to tell me what to say!**");
-	  }
+		}
 
 		if (!chanId) {
 			message.delete();
 			thingToSay = args.join(" ");
 			message.channel.send(thingToSay);
-	  } else {
-	    thingToSay = args.slice(1).join(" ");
-	    chanId.send(thingToSay);
-	    message.channel.send("💖 **Message sent.**");
-	  }
-
-	},
+		} else {
+			thingToSay = args.slice(1).join(" ");
+			chanId.send(thingToSay);
+			message.channel.send("💖 **Message sent.**");
+		}
+	}
 };
