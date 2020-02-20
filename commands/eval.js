c309315e01f6cd8b0f75459c77375d792d2a11dc
Amber Holly <halcyonsilver@gmail.com>
2020-02-20 18:18:57 +0000

    Setup pre-commit hook for linting
    - Add pre-commit as a dev dependency
    - Update eslint configuration file
    - Fix linting errors
    - Update .gitignore as KDE now creates .directory files

--- commands/eval.js
+++ commands/eval.js
@@ -1,32 +1,32 @@
 module.exports = {
-	name: 'eval',
-	description: 'Evaluates arbitrary JavaScript.',
+	name: "eval",
+	description: "Evaluates arbitrary JavaScript.",
 	cooldown: 0,
 	guildOnly: true,
 	execute(message, args) {
-
 		const clean = text => {
-	  	if (typeof(text) === "string")
-	    	return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-	  	else
-	      return text;
-		}
+			if (typeof text === "string") {
+				return text
+					.replace(/`/g, "`" + String.fromCharCode(8203))
+					.replace(/@/g, "@" + String.fromCharCode(8203));
+			} else {
+				return text;
+			}
+		};
 
 		if (message.author.id !== "190917462265430016") {
-			return message.channel.send('❣ **You can\'t use that command.**');
+			return message.channel.send("❣ **You can't use that command.**");
 		}
 
 		try {
 			const code = args.join(" ");
 			let evaled = eval(code);
 
-			if (typeof evaled !== "string")
-				evaled = require("util").inspect(evaled);
+			if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
 
-			message.channel.send(clean(evaled), { code:"xl" });
+			message.channel.send(clean(evaled), { code: "xl" });
 		} catch (err) {
 			message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
 		}
-
-	},
+	}
 };
