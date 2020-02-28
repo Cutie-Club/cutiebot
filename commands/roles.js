25cb6a374e2ce7c5411cc4db1ae141a58ded48d9
Obosob <obosob@riseup.net>
2020-02-28 17:16:41 +0000

    Lint all the things!

--- commands/roles.js
+++ commands/roles.js
@@ -9,14 +9,14 @@ module.exports = {
 	guildOnly: true,
 	execute(message) {
 
-        let roles = Array.from(message.guild.roles.values());
-        let assignableRoles = roles
-            .filter(role=>!roleBlacklist.includes(role.name))
-            .map(role=>role.name).slice(1).sort();
+		let roles = Array.from(message.guild.roles.values());
+		let assignableRoles = roles
+			.filter(role=>!roleBlacklist.includes(role.name))
+			.map(role=>role.name).slice(1).sort();
 
-        let assignableRoleNames = `\`${assignableRoles.join("`, `")}\``;
+		let assignableRoleNames = `\`${assignableRoles.join("`, `")}\``;
 
-        return message.channel.send(`💖 **Here's a list of all the available roles, cutie:**\n${assignableRoleNames}`)
+		return message.channel.send(`💖 **Here's a list of all the available roles, cutie:**\n${assignableRoleNames}`)
 
-		}
-	};
+	}
+};
