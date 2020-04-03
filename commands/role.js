ab5ad1951a127b21652719d12af6c1f4e934fee6
Amber Holly <halcyonsilver@gmail.com>
2020-04-03 23:29:39 +0100

    Update role command to catch Unhandled Promise Rejections correctly

--- commands/role.js
+++ commands/role.js
@@ -14,11 +14,15 @@ module.exports = {
 		let chosenRole;
 
 		if (!guildSettings.role_cmds) {
-			return message.channel.send({ embed: embed("❣ **Role commands are disabled.**") });
+			return message.channel.send({
+				embed: embed("❣ **Role commands are disabled.**")
+			});
 		}
-
+		
 		if (!message.channel.permissionsFor(message.client.user).has("MANAGE_ROLES", false)) {
-			return message.channel.send({ embed: embed("❣ **I don't have permission to manage roles.**") });
+			return message.channel.send({
+				embed: embed("❣ **I don't have permission to manage roles.**")
+			});
 		}
 
 		for (let role of message.guild.roles.cache.values()) {
@@ -26,35 +30,47 @@ module.exports = {
 				chosenRole = role;
 			}
 		}
-
-		if (!userInput) {
-			return message.channel.send({ embed: embed("❣ **You have to specify a role.**") } );
-		}
-
-		if (!chosenRole) {
-			return message.channel.send({ embed:
-				embed("❣ **I can't find that role. Did you type it correctly?**")
-			});
-		}
-
+		
 		if (guildSettings.role_blacklist) {
-			if (guildSettings.role_blacklist.includes(chosenRole.name)) {
+			if (guildSettings.role_blacklist.includes(chosenRole.id)) {
 				return message.channel.send({ embed: embed("💔 **That role isn't self-assignable.**") });
 			}
 		}
 
+		if (!userInput) {
+			return message.channel.send({
+				embed: embed("❣ **You have to specify a role.**")
+			});
+		}
+
+		if (!chosenRole) {
+			return message.channel.send({
+				embed: embed("❣ **I can't find that role. Did you type it correctly?**")
+			});
+		}
+
+		const failedPermission = () => message.channel.send({
+			embed: embed("💔 **I don't have the correct permissions to do that.**")
+		});
+
 		if (message.member.roles.cache.has(chosenRole.id)) {
 			message.member.roles
 				.remove(chosenRole, reason)
-				.then(
-					message.channel.send({ embed: embed(`💖 \`${chosenRole.name}\` **role removed.**`) })
-				);
+				.then(() => {
+					message.channel.send({
+						embed: embed(`💖 \`${chosenRole.name}\` **role removed.**`)
+					});
+				})
+				.catch(failedPermission);
 		} else {
 			message.member.roles
 				.add(chosenRole, reason)
-				.then(
-					message.channel.send({ embed: embed(`💖 \`${chosenRole.name}\` **role added.**`) })
-				);
+				.then(() => {
+					message.channel.send({
+						embed: embed(`💖 \`${chosenRole.name}\` **role added.**`)
+					});
+				})
+				.catch(failedPermission);
 		}
 	}
 };
