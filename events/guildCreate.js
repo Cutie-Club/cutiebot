ccae4b526595691d73b5b6d2c8cbe3144c7c4697
Amber Holly <halcyonsilver@gmail.com>
2020-11-28 21:17:36 +0000

    Fix missing guild owner in guildCreate event

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,14 +2,17 @@ const db = require("../utils/database.js");
 const settings = require("../utils/settings.js");
 
 module.exports = (client, guild) => {
-	log.info(`Joined ${guild.name}, owned by ${guild.owner.user.username}.`);
+	guild.members.fetch({ user: guild.ownerID }).then((owner) => {
+		console.log(owner);
+		log.info(`Joined ${guild.name}, owned by ${owner.user.username}.`);
 
-	db.prepare("INSERT INTO settings (guild_id) VALUES (?);").run(guild.id);
-	settings.init(client.guilds.cache.array());
+		db.prepare("INSERT INTO settings (guild_id) VALUES (?);").run(guild.id);
+		settings.init(client.guilds.cache.array());
 
-	log.info(`Created settings for ${guild.name}`);
+		log.info(`Created settings for ${guild.name}`);
 
-	client.user.setActivity(`${client.guilds.cache.size} server${client.guilds.cache.size !== 1 ? "s" : ""}.`, { type: "WATCHING" })
-		.then(activity => log.info(`Set activity to ${activity.activities[0].type} ${activity.activities[0].name}`))
-		.catch(log.error);
+		client.user.setActivity(`${client.guilds.cache.size} server${client.guilds.cache.size !== 1 ? "s" : ""}.`, { type: "WATCHING" })
+			.then(activity => log.info(`Set activity to ${activity.activities[0].type} ${activity.activities[0].name}`))
+			.catch(log.error);
+	});
 };
