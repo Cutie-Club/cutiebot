aeab959727b25b1adaf9e09d484b07b95deafdb4
Amber Holly <github@amber.vision>
2021-08-09 16:54:42 +0100

    Prevent attempting to start without token set

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ loggerInit();
 log.time("startup");
 log.info("Starting Cutiebot!");
 
+if (process.env.DISCORD_TOKEN === undefined) {
+	log.error("Token not provided. Set the DISCORD_TOKEN environment variable and restart.");
+	process.exit(1);
+}
+
 const Discord = require("discord.js");
-const token = process.env.DISCORD_TOKEN;
 const { Client, Intents } = require('discord.js');
+const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({
 	intents: [
