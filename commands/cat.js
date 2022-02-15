2b4d8e1925c82924006124224e3822ac77501648
Amber Holly <amber@amber.vision>
2022-02-15 15:48:35 +0000

    Update cat command

--- commands/cat.js
+++ commands/cat.js
@@ -1,32 +1,39 @@
-const { getRequest } = require("../utils/webRequest.js");
-// const embed = require("../utils/embed.js");
-const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const embed = require('../utils/embed.js');
+const { getRequest } = require('../utils/webRequest.js');
 
 module.exports = {
-	name: "cat",
-	description: "Cat as a service",
-	cooldown: 15,
-	guildOnly: false,
-	execute(message, args) {
-		let tags = '';
+	data: new SlashCommandBuilder()
+		.setName('cat')
+		.setDescription('Cat as a service.')
+		.addStringOption(option => 
+			option.setName('tag')
+				.setDescription('Search term for specific cat images')
+		),
+	async execute(interaction) {
+		await interaction.deferReply();
 
-		if (args.length > 0) {
-			tags = `/${args[0]}?json=true`;
-		} else {
-			tags = `?json=true`;
-		}
-		
-		const catEmbed = new MessageEmbed().setDescription(`😻 **Here's your cat!**`);
+		let catEmbed = embed('😻 **Here\'s your cat!**');
+		const string = interaction.options.getString('tag');
 
-		getRequest(`https://cataas.com/cat${tags}`)
-			.then((data) => {
-				catEmbed.setImage(`https://cataas.com${data.url}`);
-				message.channel.send({
-					embeds: [catEmbed]
-				});
-			})
-			.catch((err) => {
-				message.channel.send(JSON.stringify(err.message));
+		let tags = '?json=true';
+		if (string != null) {
+			tags = `/${string}?json=true`;
+			catEmbed.setFooter({
+				text: `tag: ${string}`
 			});
-	}
+		}
+
+		await getRequest(`https://cataas.com/cat${tags}`)
+			.then(data => {
+				catEmbed.setImage(`https://cataas.com${data.url}`);
+			})
+			.catch(err => {
+				log.error(err);
+			});
+
+		await interaction.editReply({
+			embeds: [catEmbed]
+		});
+	},
 };
