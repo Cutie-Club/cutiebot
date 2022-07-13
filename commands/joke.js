d5441a0bf8754ebc630454af0a8a338bfa5610c6
Amber Holly <amber@amber.vision>
2022-07-13 21:33:35 +0100

    Add joke command, update discordjs/builders

---
+++ commands/joke.js
@@ -0,0 +1,89 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const embed = require('../utils/embed.js');
+const { getRequest } = require('../utils/webRequest.js');
+
+const jokeCategories = [
+	'Any',
+	'Misc',
+	'Programming',
+	'Dark',
+	'Pun',
+	'Spooky',
+	'Christmas'
+];
+
+const choices = jokeCategories.map(item => ({ name: item, value: item }));
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('joke')
+		.setDescription('Fetch a joke from jokeapi.dev')
+		.addStringOption((option) =>
+			option
+				.setName('category')
+				.setDescription('Choose a category of joke.')
+				.addChoices(...choices)
+		)
+		.addStringOption((option) =>
+			option
+				.setName('type')
+				.setDescription('Choose a type of joke.')
+				.addChoices(
+					{ name: 'One-liner', value: 'single' },
+					{ name: 'Two-parter', value: 'twopart' }
+				)
+		),
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		let jokeEmbed = embed("😹 **Here's your joke!**");
+		const category = interaction.options.getString('category');
+		const jokeType = interaction.options.getString('type');
+
+		// always filter out racist and sexist jokes. ew.
+		// if the channel isnt marked nsfw, blacklist nsfw.
+		let parameters = `?format=json&?blacklistFlags=racist+sexist+religious+explicit${interaction.channel.nsfw ? '+nsfw' : ''}&safe-mode${jokeType != '' ? `&type=${jokeType}` : ''}`;
+
+		try {
+			const data = await getRequest(
+				`https://v2.jokeapi.dev/joke/${category != null ? category : 'Any'}${parameters}`
+			);
+
+			if (data.error) throw new Error(data.message, { cause: data.additionalInfo });
+
+			switch (data.type) {
+				case 'single':
+					jokeEmbed.addField('Joke', data.joke);
+					break;
+				case 'twopart':
+					jokeEmbed.addFields([
+						{ name: 'Setup', value: data.setup },
+						{ name: 'Punchline', value: `||${data.delivery}||` }
+					]);
+					break;
+			}
+
+			jokeEmbed.setFooter({
+				text: `Category: ${data.category}`
+			});
+
+			await interaction.editReply({
+				embeds: [jokeEmbed],
+			});
+		} catch (error) {
+			log.error(error);
+
+			let errorReply = `🔎 **${error.message}**`;
+			let errorEmbed = embed(errorReply);
+
+			if (error.cause) {
+				errorEmbed.addField('Cause:', error.cause);
+			}
+
+			await interaction.editReply({
+				embeds: [errorEmbed],
+				ephemeral: true,
+			});
+		}
+	},
+};
