3e6f78d0fd32e6a22b7a9bc3d57ddfe5f5fe3d37
Amber Holly <amber@amber.vision>
2022-07-21 21:08:10 +0100

    Use the logger function

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -10,7 +10,7 @@ module.exports = {
 		try {
 			await command.execute(interaction);
 		} catch (error) {
-			console.error(error);
+			log.error(error);
 			await interaction.reply({
 				content: 'There was an error while executing this command!',
 				ephemeral: true,
